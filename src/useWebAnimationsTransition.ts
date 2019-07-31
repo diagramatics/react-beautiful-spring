1d69afd39411bffad375f871a8e6ebcfd56215f1
Steven Sinatra <steven@stevensinatra.id>
2019-07-31 23:55:02 +1000

    More settings around linting

--- src/useWebAnimationsTransition.ts
+++ src/useWebAnimationsTransition.ts
@@ -1,49 +1,80 @@
 import * as React from 'react';
 
-/**
- * Get the intermediate style of an element.
- * @param node The node of the element.
- * @param referenceKeyframe The reference keyframe to pull data on what is being animated from.
- * This is to narrow down the style object we send back to a handful of properties that are involved
- * in the animation. This can be the last keyframe of the previously played animation.
- */
-const getIntermediateStyles = (node, referenceKeyframe) => {
+function convertKeyframeToCSSProperties(
+  keyframe: Keyframe,
+): React.CSSProperties {
   const {
     easing: _easing,
     composite: _composite,
     offset: _offset,
     ...cssProperties
-  } = referenceKeyframe;
+  } = keyframe;
 
   if (cssProperties.cssOffset) {
     cssProperties.offset = cssProperties.cssOffset;
     delete cssProperties.cssOffset;
   }
 
-  const computedStyle = window.getComputedStyle(node);
-  const styleObject = Object.keys({ ...cssProperties })
-    .reduce((obj, property) => {
-      obj[property] = computedStyle[property];
+  return cssProperties;
+}
+
+interface IntermediateStyles {
+  [k: string]: string | number;
+}
+/**
+ * Get the intermediate style of an element.
+ * @param element The element.
+ * @param referenceKeyframe The reference keyframe to pull data on what is being animated from.
+ * This is to narrow down the style object we send back to a handful of properties that are involved
+ * in the animation. This can be the last keyframe of the previously played animation.
+ */
+const getIntermediateStyles = (
+  element: HTMLElement,
+  referenceKeyframe: Keyframe,
+): IntermediateStyles => {
+  const cssProperties = convertKeyframeToCSSProperties(referenceKeyframe);
+
+  const computedStyle = window.getComputedStyle(element);
+  const styleObject = Object.keys(cssProperties).reduce<IntermediateStyles>(
+    (obj, property): IntermediateStyles => {
+      if (Object.prototype.hasOwnProperty.call(computedStyle, property)) {
+        obj[property] = computedStyle[property as keyof CSSStyleDeclaration];
+      }
       return obj;
-    }, {});
+    },
+    {},
+  );
 
   return styleObject;
 };
 
-export const useWebAnimationsTransition = (show, config) => {
+type Config = {
+  ref: React.RefObject<HTMLElement>;
+  from: Keyframe;
+  enter: Keyframe;
+  leave: Keyframe;
+  duration: number;
+};
+
+export const useWebAnimationsTransition = (
+  show: boolean,
+  config: Config,
+): React.CSSProperties => {
   const { ref: nodeRef, from, enter, leave, duration } = config;
 
   const previousShow = React.useRef(show);
-  const [style, setStyle] = React.useState(() => from);
+  const [style, setStyle] = React.useState<React.CSSProperties>(() =>
+    convertKeyframeToCSSProperties(from),
+  );
   const [isFinished, setIsFinished] = React.useState(true);
-  const animationObject = React.useRef();
-  const animationLastKeyframe = React.useRef();
+  const animationObject = React.useRef<Animation>();
+  const animationLastKeyframe = React.useRef<Keyframe>();
 
-  const onFinish = () => {
+  const onFinish = (): void => {
     setIsFinished(true);
     animationLastKeyframe.current = undefined;
     console.log('finish');
-  }
+  };
 
   React.useLayoutEffect(() => {
     if (show !== previousShow.current) {
@@ -55,18 +86,21 @@ export const useWebAnimationsTransition = (show, config) => {
       if (show === true) {
         const beforeStyles = isFinished
           ? from
-          : getIntermediateStyles(nodeRef.current, animationLastKeyframe.current || from);
+          : getIntermediateStyles(
+              nodeRef.current,
+              animationLastKeyframe.current || from,
+            );
         console.log(beforeStyles);
-        setStyle(enter);
+        setStyle(convertKeyframeToCSSProperties(enter));
         // Cancel previous animation after we get the intermediate style.
         if (animationObject.current) {
           animationObject.current.cancel();
           animationObject.current = undefined;
         }
-        const animation = nodeRef.current.animate([
-          beforeStyles,
-          enter,
-        ], { duration, fill: 'both' });
+        const animation = nodeRef.current.animate([beforeStyles, enter], {
+          duration,
+          fill: 'both',
+        });
         setIsFinished(false);
         animationObject.current = animation;
         animationLastKeyframe.current = enter;
@@ -74,26 +108,28 @@ export const useWebAnimationsTransition = (show, config) => {
       } else if (show === false) {
         const beforeStyles = isFinished
           ? enter
-          : getIntermediateStyles(nodeRef.current, animationLastKeyframe.current || enter);
+          : getIntermediateStyles(
+              nodeRef.current,
+              animationLastKeyframe.current || enter,
+            );
         console.log(beforeStyles);
-        setStyle(leave);
+        setStyle(convertKeyframeToCSSProperties(leave));
         // Cancel previous animation after we get the intermediate style.
         if (animationObject.current) {
           animationObject.current.cancel();
           animationObject.current = undefined;
         }
-        const animation = nodeRef.current.animate([
-          beforeStyles,
-          leave,
-        ], { duration, fill: 'both' });
+        const animation = nodeRef.current.animate([beforeStyles, leave], {
+          duration,
+          fill: 'both',
+        });
         setIsFinished(false);
         animationObject.current = animation;
         animationLastKeyframe.current = leave;
         animation.addEventListener('finish', onFinish);
       }
     }
-  }, [show]);
-
+  }, [duration, enter, from, isFinished, leave, nodeRef, show]);
 
   return style;
 };
--- stories/useWebAnimationTransition.stories.tsx
+++ stories/useWebAnimationTransition.stories.tsx
@@ -2,74 +2,92 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { useWebAnimationsTransition } from '../src/useWebAnimationsTransition';
 
-storiesOf('WebAnimationTransition', module)
-  .add('Hook', () => {
-    function useInterval(callback: Function, delay: number) {
-      const savedCallback = React.useRef<Function>();
+storiesOf('WebAnimationTransition', module).add('Hook', () => {
+  function useInterval(callback: Function, delay: number) {
+    const savedCallback = React.useRef<Function>();
 
-      // Remember the latest callback.
-      React.useEffect(() => {
-        savedCallback.current = callback;
-      }, [callback]);
+    // Remember the latest callback.
+    React.useEffect(() => {
+      savedCallback.current = callback;
+    }, [callback]);
 
-      // Set up the interval.
-      React.useEffect(() => {
-        function tick() {
-          if (savedCallback.current) {
-            savedCallback.current();
-          }
+    // Set up the interval.
+    React.useEffect(() => {
+      function tick() {
+        if (savedCallback.current) {
+          savedCallback.current();
         }
-        if (delay !== null) {
-          let id = setInterval(tick, delay);
-          return () => clearInterval(id);
-        }
-      }, [delay]);
-    }
+      }
+      if (delay !== null) {
+        const id = setInterval(tick, delay);
+        return () => clearInterval(id);
+      }
+    }, [delay]);
+  }
 
-    const Component = () => {
-      const [show, setShow] = React.useState(false);
-      const [run, setRun] = React.useState(false);
-      const nodeRef = React.useRef();
-      const style = useWebAnimationsTransition(show, {
-        ref: nodeRef,
-        from: { background: 'black', transform: 'scale(1)', easing: 'ease-in-out' },
-        enter: { background: 'black', transform: 'scale(1.25)', easing: 'ease-in-out' },
-        leave: { background: 'black', transform: 'scale(1)', easing: 'ease-in-out' },
-        duration: 500,
-      });
+  const Component = () => {
+    const [show, setShow] = React.useState(false);
+    const [run, setRun] = React.useState(false);
+    const nodeRef = React.useRef<HTMLDivElement>(null);
+    const style = useWebAnimationsTransition(show, {
+      ref: nodeRef,
+      from: {
+        background: 'black',
+        transform: 'scale(1)',
+        easing: 'ease-in-out',
+      },
+      enter: {
+        background: 'black',
+        transform: 'scale(1.25)',
+        easing: 'ease-in-out',
+      },
+      leave: {
+        background: 'black',
+        transform: 'scale(1)',
+        easing: 'ease-in-out',
+      },
+      duration: 500,
+    });
 
-      useInterval(() => {
-        run && setShow(!show);
-      }, 500);
+    useInterval(() => {
+      run && setShow(!show);
+    }, 500);
 
-      return (
-        <>
-          <button onClick={() => setRun(!run)}>Toggle</button>
-          <div
-            style={{
-              background: 'black',
-              width: '50px',
-              height: '50px',
-              ...style,
-            }}
-            ref={nodeRef}
-            onMouseOver={() => setShow(true)}
-            onMouseOut={() => setShow(false)}
-          ></div>
-          <style dangerouslySetInnerHTML={{ __html: `
-            .a.b {
-              transform: scale(1.25);
-            }
-          `}} />
-          <div className={`a ${show && 'b'}`} style={{
+    return (
+      <>
+        <button onClick={() => setRun(!run)}>Toggle</button>
+        <div
+          style={{
             background: 'black',
             width: '50px',
             height: '50px',
-            transition: 'all 0.5s ease-in-out'
-          }}></div>
-        </>
-      )
-    }
+            ...style,
+          }}
+          ref={nodeRef}
+          onMouseOver={() => setShow(true)}
+          onMouseOut={() => setShow(false)}
+        ></div>
+        <style
+          dangerouslySetInnerHTML={{
+            __html: `
+            .a.b {
+              transform: scale(1.25);
+            }
+          `,
+          }}
+        />
+        <div
+          className={`a ${show && 'b'}`}
+          style={{
+            background: 'black',
+            width: '50px',
+            height: '50px',
+            transition: 'all 0.5s ease-in-out',
+          }}
+        ></div>
+      </>
+    );
+  };
 
-    return <Component />;
-  });
+  return <Component />;
+});
